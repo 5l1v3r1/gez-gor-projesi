3e3214484e6348089b24654f8c4480a2da8a8fed
Alperen Bozkurt <bozkurttalperen@gmail.com>
2019-06-04 00:43:05 +0300

    kullanıcı giriş yaptıkttan sonra anasayfaya erişimi engellendi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
-  def index
+  before_action :check_user, only: [ :index ]
 
+  def index
     render layout: false
+  end
+
+  def check_user
+    redirect_to(maps_path) if user_signed_in? 
   end
 end
