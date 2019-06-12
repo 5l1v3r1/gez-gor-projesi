e5247fa6bc70d72de3d38defb44a9220c11f5c79
simgesen <simge.sen@bil.omu.edu.tr>
2019-06-12 23:06:11 +0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/maps/:city_id', to: 'maps#show'
-  resources :maps, only: [:index, :new, :create, :show]
+  resources :maps, only: [:index, :new, :create]
+  
   root to: 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
